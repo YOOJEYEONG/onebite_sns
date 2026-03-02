c73a2d62c1fad2c2239dee248297ae9af742a8c0
YooJeYoung <jdngh@naver.com>
2026-03-02 13:54:40 +0900

    React Router V7

--- onebite-exam/src/App.tsx
+++ onebite-exam/src/App.tsx
@@ -1,134 +1,27 @@
-import { ChefHat } from "lucide-react";
-import { toast } from "sonner";
+import { Outlet, Route, Routes } from "react-router";
 import "./App.css";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "./components/ui/alert-dialog";
-import { Button } from "./components/ui/button";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./components/ui/carousel";
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
-} from "./components/ui/dialog";
-import { Input } from "./components/ui/input";
-import {
-  Popover,
-  PopoverContent,
-  PopoverDescription,
-  PopoverHeader,
-  PopoverTitle,
-  PopoverTrigger,
-} from "./components/ui/popover";
-import { Toaster } from "./components/ui/sonner";
-import { Textarea } from "./components/ui/textarea";
-import { cn } from "./lib/utils";
+import IndexPage from "./pages/index-page";
+import SignInPage from "./pages/sign-in-page";
+import SignUpPage from "./pages/sign-up-page";
+
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
-  const isActive = true;
-
   return (
-    <div className="p-5">
-      <ChefHat />
-
-      <Carousel className="mx-10">
-        <CarouselContent>
-          <CarouselItem className="basis-1/3">1</CarouselItem>
-          <CarouselItem className="basis-1/3">2</CarouselItem>
-          <CarouselItem className="basis-1/3">3</CarouselItem>
-          <CarouselItem className="basis-1/3">4</CarouselItem>
-          <CarouselItem className="basis-1/3">5</CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button>Popover</Button>
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverHeader>
-            <PopoverTitle>Title</PopoverTitle>
-            <PopoverDescription>text</PopoverDescription>
-          </PopoverHeader>
-        </PopoverContent>
-      </Popover>
-
-      <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
-          {/* <DialogHeader> */}
-          <DialogTitle>Title</DialogTitle>
-          {/* <DialogDescription>Description</DialogDescription>
-          </DialogHeader> */}
-          <div>body</div>
-        </DialogContent>
-      </Dialog>
-
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="outline">Show Dialog</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-
-      <Toaster />
-
-      <Textarea />
-
-      <Input placeholder="입력해주세요" />
-
-      <Button
-        onClick={() => {
-          toast("토스트 메세지", {
-            position: "top-center",
-          });
-        }}
-      >
-        딸깍
-      </Button>
-      <Button variant={"destructive"}>딸깍</Button>
-      <Button variant={"ghost"}>딸깍</Button>
-      <Button variant={"link"}>딸깍</Button>
-      <Button variant={"outline"}>딸깍</Button>
-      <Button variant={"secondary"}>딸깍</Button>
-
-      <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
-        isActive
-      </div>
-
-      <div className="text-primary">Primary</div>
-      <div className="text-muted">Muted</div>
-      <div className="text-destructive">Destructive</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 
